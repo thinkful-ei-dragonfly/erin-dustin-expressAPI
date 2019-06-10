9afc34bcb2d1e32701d19acab5fffc9651665926
Erin Dustin <erincdustin@gmail.com>
2019-06-10 14:23:46 -0500

    third drill working

--- app.js
+++ app.js
@@ -35,6 +35,29 @@ app.get('/cipher', (req,res) => {
   res.send(mappedText);
 });
 
+app.get('/lotto', (req,res) => {
+  const arr = req.query.arr.map(num => parseInt(num));
+  const random = arr.map(() => Math.floor(Math.random()*20));
+  // const random = [9,2,3,4,5,6]
+  const matches = random.reduce((total,num) => {
+    if (arr.includes(num)){
+      total ++;
+    }
+    return total;
+  }, 0);
+  // const matches = 6;
+
+  if (matches <4) {
+    res.send('Sorry, you lose');
+  } else if (matches === 4){
+    res.send('Congratulations, you win a free ticket');
+  } else if (matches === 5) {
+    res.send('Congratulations! You win $100!');
+  } else {
+    res.send('Wow! Unbelievable! You could have won the mega millions!');
+  }
+});
+
 app.listen(8001, () => {
   console.log('working on 8001');
 });
