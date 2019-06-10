366b72e17c77953eda1698e99e4a7aca8c40a001
Erin Dustin <erincdustin@gmail.com>
2019-06-10 14:42:30 -0500

    added error handling

--- app.js
+++ app.js
@@ -8,17 +8,34 @@ const app = express();
 app.use(morgan('dev'));
 
 app.get('/sum', (req,res) => {
-  const a = parseInt(req.query.a);
-  const b = parseInt(req.query.b);
-  const c = a + b;
+  const aValue = parseInt(req.query.a);
+  const bValue = parseInt(req.query.b);
 
-  res.send(`The sum of ${a} and ${b} is ${c}`);
+  if(!aValue || Number.isNaN(aValue)) {
+    return res.status(400).send('a is required and should be a number');
+  }
+  if(!bValue || Number.isNaN(bValue)) {
+    return res.status(400).send('b is required and should be a number');
+  }
+
+  const c = aValue + bValue;
+
+  res.send(`The sum of ${aValue} and ${bValue} is ${c}`);
 
 });
+
 
 app.get('/cipher', (req,res) => {
   const text = req.query.text.toLowerCase();
   const shift = parseInt(req.query.shift);
+
+  if(!text) {
+    return res.status(400).send('text is required');
+  }
+  if(!shift || Number.isNaN(shift)) {
+    return res.status(400).send('shift is required and needs to be a number');
+  }
+ 
   const string = text.split('');
   // const number = String.
 
@@ -35,9 +52,21 @@ app.get('/cipher', (req,res) => {
   res.send(mappedText);
 });
 
+
+
 app.get('/lotto', (req,res) => {
-  const arr = req.query.arr.map(num => parseInt(num));
-  const random = arr.map(() => Math.floor(Math.random()*20));
+  const arr = req.query.arr;
+
+  if(!arr) {
+    return res.status(400).send('arr is required');
+  }
+  if(arr.length < 6 || arr.length > 6){
+    return res.status(400).send('arr must have 6 values');
+  }
+
+  const mappedArr = arr.map(num => parseInt(num));
+  
+  const random = mappedArr.map(() => Math.floor(Math.random()*20));
   // const random = [9,2,3,4,5,6]
   const matches = random.reduce((total,num) => {
     if (arr.includes(num)){
